680bc21d425a33bf78b99068a0b352b96f24deff
nainav <naina.verma7@gmail.com>
2017-06-12 13:23:39 +0530

    taget url

--- tests/api/apiCalls.js
+++ tests/api/apiCalls.js
@@ -1,6 +1,12 @@
 'use strict';
 let frisby = require("frisby");
 let token = process.env['OSIO_TOKEN']
+let URL = process.env['targetURL']
+let url;
+// URL = 'https://recommender.api.openshift.io/api/v1/';
+ 
+
+
 frisby.globalSetup({
   request: {
     headers: { 'Content-Type': 'application/json',
@@ -11,11 +17,18 @@ frisby.globalSetup({
 class apiCalls {
 
   constructor() {
+    if(URL=== undefined){
+       url = 'https://recommender.api.openshift.io/api/v1/';
+      }
+
+    else {
+       url = URL;
+      }
   }
 
  GetComponentAnalysis (parameter) {
-        var analytics = frisby.create('Connecting to REST API - endpoint');
-        analytics.get("https://recommender.api.openshift.io/api/v1/component-analyses/" + parameter).expectStatus(200)
+        var analytics = frisby.create('Connecting to REST API - endpoint :: ' + url +parameter);
+        analytics.get(url + "component-analyses/" + parameter).expectStatus(200)
         return analytics;
  
   }
